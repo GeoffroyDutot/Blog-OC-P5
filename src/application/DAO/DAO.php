b6a804d9e2a7d1cf70543f42add9c3badf1336dd
geoffroy <geoffroy.dutot@jymeo.net>
2020-07-28 18:03:09 +0200

    Config Connect DB

---
+++ src/application/DAO/DAO.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\DAO;
+
+use App\Core\Config;
+
+class DAO
+{
+    private Config $config;
+    private $db = null;
+
+    public function __construct() {
+        $this->config = Config::getInstance();
+    }
+
+    public function connectDb()
+    {
+        if (null !== $this->db) {
+          return $this->db;
+        }
+        try {
+
+            $this->db = new \PDO("mysql:host=".$this->config->getParam('hostname').";dbname=".$this->config->getParam('database'), $this->config->getParam('username'), $this->config->getParam('password'));
+            $this->db->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+            return $this->db;
+        } catch (Exception $e) {
+            die('Erreur : ' . $e->getMessage());
+        }
+    }
+}
