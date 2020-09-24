f71bb90d96b2376c59a49d82e5e8d36e3b844b92
geoffroy <geoffroy.dutot@jymeo.net>
2020-09-24 18:41:05 +0200

    Redirect fonction

--- src/application/helper/url_helper.php
+++ src/application/helper/url_helper.php
@@ -1,1 +1,15 @@
 <?php
+
+
+namespace App\Helper;
+
+
+trait url_helper {
+    function redirect(string $url, $permanent = false) {
+        if ($permanent) {
+            header('HTTP/1.1 301 Moved Permanently');
+        }
+        header('Location: '.$url);
+        exit();
+    }
+}
